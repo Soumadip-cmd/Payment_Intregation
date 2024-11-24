fad36041b073aa1a22e55dd350429053b3b98981
Soumadip-cmd <soumadipsantra2004@gmail.com>
2024-11-24 21:53:22 +0530

    updared nfh

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 const mongoose =require('mongoose')
-const mongoUrl="mongodb://localhost:27017/Payment"
+const mongoUrl="mongodb+srv://soumadipsantra2004:s5iZmx5gbkM2u21y@portfolio.mkxd3.mongodb.net/Payment"
 
 const connectedDb=()=>{
       mongoose.connect(mongoUrl);
